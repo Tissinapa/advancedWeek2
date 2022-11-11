9afdec308f0eeb3ab8011a135f7d015f6fb96d54
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2022-11-11 14:34:10 +0200

    task 4 done

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 let hellomes = {
     msg: "Hello world"
 }
+let nums = {
+    numbers: [1, 2, 3 ]
+}
 
 
 
 
 const express = require("express");
 const app = express()
+const path = require("path")
 const port = 3000
 
 app.get("/hello",(req, res) =>{
@@ -19,14 +23,16 @@ app.get("/echo/:id",(req, res) =>{
 
 
 app.post("/sum",(req, res) =>{
-    let numbers = [1, 2, 3]
+    let numbers = [1, 2, 3 ,4]
     let answer = 0
+    //nums.push(req.body.numbers)
     for (let i of numbers){
         answer += i
     }
-    console.log(answer)
+    
     res.send({sum: answer})
 })
+app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/", (req , res) => {
     res.send("Testing if this works or not");
